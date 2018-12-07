086c16a90ed91e22aba234e2f0f47d7a60f0a3b0
Joey Kilgore <joey.a.kilgore@gmail.com>
2018-12-07 14:36:23 -0500

    Added ability to change number of processes generated
    The bounds for each process are caculated individually to allow the user to choose how many processes they want running.

--- Python Scripts/MultiThreadTest.py
+++ Python Scripts/MultiThreadTest.py
@@ -1,14 +1,34 @@
 import sys
 import subprocess
+import time
+import math
+
+def roundDownToRes(num, res):
+    return res * math.floor(num/res)
+
+def roundUpToRes(num, res):
+    return res * math.ceil(num/res)
+
+start = time.clock()
 
 procs = []
-for i in range(5):
-    lowBound = i*100000
-    upperBound = lowBound + 50000
-    resolution = 50000
-    argList = [str(lowBound), str(upperBound), str(resolution)]
-    proc = subprocess.Popen(['python', 'C:/Users/Joey/Desktop/Stuff/Code/Python/RunHoc.py'] + argList)
+numProcesses = 6
+maxBound = 300000
+minBound = 0
+resolution = 10000
+
+for i in range(numProcesses):
+    upperBound = roundDownToRes((maxBound - minBound)/numProcesses * (i+1), resolution) + minBound
+    lowerBound = roundUpToRes((maxBound - minBound)/numProcesses * i, resolution) + minBound
+    argList = [str(lowerBound), str(upperBound), str(resolution)]
+    proc = subprocess.Popen(['python', '"C:/Users/Joey/Desktop/Stuff/Code/NEURON/Python Scripts/RunHoc.py"'] + argList)
     procs.append(proc)
+
+
 
 for proc in procs:
     proc.wait()
+
+stop = time.clock()
+
+print(stop - start)
