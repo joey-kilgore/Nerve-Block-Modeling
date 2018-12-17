93c4afd672356c3c728d661e76bd123dfeb87763
Joey Kilgore <joey.a.kilgore@gmail.com>
2018-12-17 15:10:52 -0500

    Created python scripts for gnabar experiment
    Two scripts were created for running numerous simulations at once. The overall experiment tests the effects of different Gnabar values on different numbers of nodes

---
+++ Python Scripts/GnabarExperiment.py
@@ -0,0 +1,24 @@
+# This is the script instantiated from GnabarMultiThread.py
+import sys
+import os
+import neuron
+
+gnabarValue = round(float(sys.argv[1]),2)
+processId = int(sys.argv[2])
+
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+my_file = os.path.join(THIS_FOLDER, 'file'+str(processId)+'.csv')
+
+sys.stdout = open(my_file, 'w')
+print('Gnabar value,' + str(gnabarValue))
+
+h = neuron.hoc.HocObject()
+
+h('chdir("C:/Users/Joey/Desktop/Stuff/Code/NEURON/Current Simulation/")')
+h('getcwd()')
+h('nrn_load_dll("nrnmech.dll")')
+h('load_file("CNOW_run.hoc")')
+
+hocCommand = "changeGnabarFromCenter(" + str(gnabarValue) + ")"
+
+h(hocCommand)
