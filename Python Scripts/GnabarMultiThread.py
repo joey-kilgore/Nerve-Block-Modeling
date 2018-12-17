7ec57d660a1bcb33f5634d672c65ef1e4ab14658
Joey Kilgore <joey.a.kilgore@gmail.com>
2018-12-17 15:10:52 -0500

    Created python scripts for gnabar experiment
    Two scripts were created for running numerous simulations at once. The overall experiment tests the effects of different Gnabar values on different numbers of nodes

---
+++ Python Scripts/GnabarMultiThread.py
@@ -0,0 +1,30 @@
+# GnabarMultiThread runs multiple simulations concurently for collecting data
+
+import sys
+import subprocess
+import time
+import math
+import os
+
+start = time.clock()
+
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+my_file = os.path.join(THIS_FOLDER, 'GnabarExperiment.py')
+
+procs = []
+numProcesses = 5
+lowerGnabar = 2.7   # starting test value for gnabar
+incrementGnabar = .15 # the increment of gnabar between multiple tests
+for i in range(numProcesses):
+    argList = [str(lowerGnabar + i*incrementGnabar), str(i)]
+    proc = subprocess.Popen(['python', my_file] + argList)
+    procs.append(proc)
+
+
+
+for proc in procs:
+    proc.wait()
+
+stop = time.clock()
+
+print(stop - start)
