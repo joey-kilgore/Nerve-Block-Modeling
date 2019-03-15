8bf0202ebaddd7f4d4279f08e5e21eed32e801c8
Joey Kilgore <joey.a.kilgore@gmail.com>
2019-03-15 13:10:02 -0400

    divided code into methods

--- Python Scripts/WaveformComparisonExperiment.py
+++ Python Scripts/WaveformComparisonExperiment.py
@@ -14,36 +14,63 @@ sys.stdout = open(my_file, 'w')
 
 h = neuron.hoc.HocObject()
 
-h('nrn_load_dll("C:/Users/Joey/Desktop/Stuff/Code/Nerve-Block-Modeling/Current Simulation/models/nrnmech.dll")')
-h('load_file("C:/Users/Joey/Desktop/Stuff/Code/Nerve-Block-Modeling/Current Simulation/CNOW_run.hoc")')
-#h('changeDiam(7)')
-h('setStim(10,40,.1)')
-h('changeModel(0)') # set MRG MODEL
-h('insert_cf()') # set freq-dep MRG MODEL
+def hocSetup():
+    h('nrn_load_dll("C:/Users/Joey/Desktop/Stuff/Code/Nerve-Block-Modeling/Current Simulation/models/nrnmech.dll")')
+    h('load_file("C:/Users/Joey/Desktop/Stuff/Code/Nerve-Block-Modeling/Current Simulation/CNOW_run.hoc")')
+    # Fiber diameter varies across each process
+    setDiam = 'changeDiam(' + str(processId+3) + ')'
+    h(setDiam)
+    h('setStim(10,40,.1)')
+    h('changeModel(0)') # set MRG MODEL
+    h('insert_cf()') # set freq-dep MRG MODEL
 
-h('waveform_sel(1)')
-h('onset1 = 10')
-h('dur1 = 300')
-h('setoffset(0)')
-h('sinestim()')
-print("SINE WAVE")
-h('run()')
-# for i in range(startingFreq,startingFreq+freqRange+1000,1000):
-#     print('FREQUENCY,'+str(i))
-#     h('freq = '+str(i))
-#     h('sinestim()')
-#     h('findThreshold(450000,650000,0,10,50,.1,1,1000)')
+def sineWaveTest():
+    h('waveform_sel(1)')
+    h('onset1 = 10')
+    h('dur1 = 300')
+    h('setoffset(0)')
+    h('sinestim()')
+    print("SINE WAVE")
+    h('run()')
+    # for i in range(startingFreq,startingFreq+freqRange+1000,1000):
+    #     print('FREQUENCY,'+str(i))
+    #     h('freq = '+str(i))
+    #     h('sinestim()')
+    #     h('findThreshold(450000,650000,0,10,50,.1,1,1000)')
 
-h('waveform_sel(2)')
-h('amp1 = 500000')
-h('onset1 = 10')
-h('dur1 = 300')
-h('offset = 0')
-h('tristim()')
-print("TRIANGLE WAVE")
-h('run()')
-# for i in range(startingFreq,startingFreq+freqRange+1000,1000):
-#     print('FREQUENCY,'+str(i))
-#     h('freq = '+str(i))
-#     h('sinestim()')
-#     h('findThreshold(450000,650000,0,10,50,.1,1,1000)')
+def triWaveTest():
+    h('waveform_sel(2)')
+    h('amp1 = 1500000')
+    h('onset1 = 10')
+    h('dur1 = 300')
+    h('offset = 0')
+    h('tristim()')
+    print("TRIANGLE WAVE")
+    h('run()')
+    # for i in range(startingFreq,startingFreq+freqRange+1000,1000):
+    #     print('FREQUENCY,'+str(i))
+    #     h('freq = '+str(i))
+    #     h('sinestim()')
+    #     h('findThreshold(450000,650000,0,10,50,.1,1,1000)')
+
+def squareWaveTest():
+    h('waveform_sel(0)')
+    # Set the square wave params to match the frequency
+    frequency = 10 #kHz
+    cathodDuration = 1/frequency/2
+    setCathodDur = 'cathod_dur=' + str(cathodDuration)
+    setPostCathodDur = 'postCathod_dur=0'
+    setAnodDur = 'anod_dur=' + str(cathodDuration)
+    setPostAnodDur = 'postAnod_dur=0'
+    h(setCathodDur)
+    h(setPostCathodDur)
+    h(setAnodDur)
+    h(setPostAnodDur)
+    h('high_amp1 = 500000')
+    h('bal_val1()')
+    h('run()')
+
+hocSetup()
+sineWaveTest()
+triWaveTest()
+squareWaveTest()
